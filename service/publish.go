9a14059456526e805a83cf952c3dd34c9a100dde
Seunghoon-Oh <steveoh5419@gmail.com>
2023-09-14 05:33:58 +0000

    주석제거

--- service/publish.go
+++ service/publish.go
@@ -21,37 +21,3 @@ func PublishMsg(subject, action string) string {
 	}
 	return "Ok"
 }
-
-// func main() {
-
-// 	url := os.Getenv("NATS_URL")
-// 	if url == "" {
-// 		url = nats.DefaultURL
-// 	}
-
-// 	nc, _ := nats.Connect(url)
-
-// 	defer nc.Drain()
-
-// 	nc.Publish("greet.joe", []byte("hello"))
-
-// 	sub, _ := nc.SubscribeSync("greet.*")
-
-// 	msg, _ := sub.NextMsg(10 * time.Millisecond)
-// 	fmt.Println("subscribed after a publish...")
-// 	fmt.Printf("msg is nil? %v\n", msg == nil)
-
-// 	nc.Publish("greet.joe", []byte("hello"))
-// 	nc.Publish("greet.pam", []byte("hello"))
-
-// 	msg, _ = sub.NextMsg(10 * time.Millisecond)
-// 	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
-
-// 	msg, _ = sub.NextMsg(10 * time.Millisecond)
-// 	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
-
-// 	nc.Publish("greet.bob", []byte("hello"))
-
-// 	msg, _ = sub.NextMsg(10 * time.Millisecond)
-// 	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
-// }
